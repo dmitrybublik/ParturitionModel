7c6f03061f4ec8d8843df74b29c4d8fd03aa6eb0
dmitry.bublik <dmitry.buba@gmail.com>
2016-02-28 20:34:48 +0200

    Added save

--- ParturitionModel.App/MainWindow.xaml.cs
+++ ParturitionModel.App/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using ParturitionModel.Core;
@@ -12,6 +14,8 @@ namespace ParturitionModel.App
     /// </summary>
     public partial class MainWindow
     {
+        const string FileName = "simulation.csv";
+
         private readonly ObservableCollection<DataViewModel> _items =
             new ObservableCollection<DataViewModel>();
 
@@ -22,6 +26,15 @@ namespace ParturitionModel.App
 
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Year;\tCount;\tChild Death");
+            File.AppendAllText(FileName, sb.ToString());
+
             ((Button) sender).IsEnabled = false;
 
             try
@@ -39,6 +52,26 @@ namespace ParturitionModel.App
             }
         }
 
+        private string BornInfosToString(IEnumerable<BornInfo> bornInfos)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var info in bornInfos)
+            {
+                sb.AppendFormat("#{0} Age: {1}; Death: {2:P2}", info.Order, info.MotherAge, info.Factor);
+            }
+
+            return sb.ToString();
+        }
+
+        private void SaveDataToFile(DataViewModel data)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0};\t{1};\t{2}", data.Year, data.Population, BornInfosToString(data.BornInfos));
+            sb.AppendLine();
+
+            File.AppendAllText(FileName, sb.ToString());
+        }
+
         public IEnumerable<DataViewModel> Items
 
         {
@@ -53,6 +86,8 @@ namespace ParturitionModel.App
                 Population = ((Simulator) sender).CurrentPopuation,
                 BornInfos = e.BornInfos.Values.OrderBy(x => x.Order)
             };
+
+            SaveDataToFile(data);
 
             await Dispatcher.InvokeAsync(() =>
             {
