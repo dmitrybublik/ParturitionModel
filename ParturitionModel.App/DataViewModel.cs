f3b4a6afc2e5fd43f2749179b2ab280abdcd86b2
dmitry.bublik <dmitry.buba@gmail.com>
2016-02-28 18:34:33 +0200

    Added autoscroll

---
+++ ParturitionModel.App/DataViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ParturitionModel.App
+{
+    public sealed class DataViewModel
+    {
+        public int Year { get; set; }
+
+        public int Population { get; set; }
+
+        public int ChildDeath { get; set; }
+
+        public int ChildBorn { get; set; }
+
+        public double ChildDeathPercents
+        {
+            get
+            {
+                if (ChildBorn == 0)
+                {
+                    return 0.0;
+                }
+
+                return (double) ChildDeath/ChildBorn;
+            }
+        }
+    }
+}
--- ParturitionModel.App/MainWindow.xaml.cs
+++ ParturitionModel.App/MainWindow.xaml.cs
@@ -57,32 +57,9 @@ namespace ParturitionModel.App
             await Dispatcher.InvokeAsync(() =>
             {
                 _items.Add(data);
+                DataGrid.ScrollIntoView(data);
             });
 
-        }
-    }
-
-    public sealed class DataViewModel
-    {
-        public int Year { get; set; }
-
-        public int Population { get; set; }
-
-        public int ChildDeath { get; set; }
-
-        public int ChildBorn { get; set; }
-
-        public double ChildDeathPercents
-        {
-            get
-            {
-                if (ChildBorn == 0)
-                {
-                    return 0.0;
-                }
-
-                return (double) ChildDeath/ChildBorn;
-            }
         }
     }
 }
